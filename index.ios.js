eaf48ff52617957425268f4230f784c3a9f89d4a
Simon Choi <siimon.choi@gmail.com>
2018-05-01 20:25:02 -0700

    init

---
+++ index.ios.js
@@ -0,0 +1,12 @@
+// Import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create a component
+const App = () => (
+    <Text>asdfasdf</Text>
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
